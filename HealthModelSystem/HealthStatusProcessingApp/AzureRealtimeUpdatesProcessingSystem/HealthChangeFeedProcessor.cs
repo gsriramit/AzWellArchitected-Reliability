7229aa701da9c9ff6fa9776fc943e003d29a7254
Sriram.G <gsriramit@gmail.com>
2021-07-24 20:32:07 +0530

    added a new function that integrates with signalr service and publishes realtime health changes

--- HealthModelSystem/HealthStatusProcessingApp/AzureRealtimeUpdatesProcessingSystem/HealthChangeFeedProcessor.cs
+++ HealthModelSystem/HealthStatusProcessingApp/AzureRealtimeUpdatesProcessingSystem/HealthChangeFeedProcessor.cs
@@ -1,7 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.Documents;
 using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 
@@ -9,19 +13,38 @@ namespace AzureRealtimeUpdatesProcessingSystem
 {
     public static class HealthChangeFeedProcessor
     {
+
+        [FunctionName("negotiate")]
+        public static SignalRConnectionInfo Negotiate(
+            [HttpTrigger(AuthorizationLevel.Function)] HttpRequest req,
+            [SignalRConnectionInfo(HubName = "systemhealthmodelhub")] SignalRConnectionInfo connectionInfo)
+        {
+            return connectionInfo;
+        }
+
+
         [FunctionName("ProcessHealthChangeFeed")]
-        public static void Run([CosmosDBTrigger(
+        public static async Task Run([CosmosDBTrigger(
             databaseName: "HealthModelDatabase",
             collectionName: "CloudComponentsHealth",
             ConnectionStringSetting = "CosmosConnectionString",
             LeaseCollectionName = "leases",
-            CreateLeaseCollectionIfNotExists = true)]IReadOnlyList<Document> input, ILogger log)
+            CreateLeaseCollectionIfNotExists = true)]IReadOnlyList<Document> healthChangeMessages,
+            [SignalR(HubName = "systemhealthmodelhub")] IAsyncCollector<SignalRMessage> signalRMessages,
+            ILogger log)
         {
-            if (input != null && input.Count > 0)
+            if (healthChangeMessages != null && healthChangeMessages.Count > 0)
             {
-                log.LogInformation("Documents modified " + input.Count);
-                log.LogInformation("First document Id " + input[0].Id);
+                log.LogInformation("Documents modified " + healthChangeMessages.Count);
+                log.LogInformation("First document Id " + healthChangeMessages[0].Id);
             }
+
+            await signalRMessages.AddAsync(
+                new SignalRMessage
+                {
+                    Target = "newMessage",
+                    Arguments = new[] { "" }
+                });
         }
     }
 }
